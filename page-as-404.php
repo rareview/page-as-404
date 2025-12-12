d0d1b7c484bab281868a8c1e6cf3c3e972a9b7d3
Max Morgan <maxinacube@gmail.com>
2025-12-12 16:39:18 -0500

    refactor: Remove need for composer autoload, add details to readme

--- page-as-404.php
+++ page-as-404.php
@@ -16,22 +16,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-// Require Composer autoloader if it exists.
-if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
-	require_once __DIR__ . '/vendor/autoload.php';
-	new PageAs404\Inc\PageAs404ServiceProvider();
-} else {
-	add_action(
-		'admin_notices',
-		function () {
-			?>
-			<div class="notice notice-error">
-				<p><?php esc_html_e( 'Page As 404: You must run "composer install" before running this plugin.', 'page-as-404' ); ?></p>
-			</div>
-			<?php
-		}
-	);
-}
+require_once __DIR__ . '/includes/Activation.php';
+require_once __DIR__ . '/includes/Helpers.php';
+require_once __DIR__ . '/includes/Register.php';
+require_once __DIR__ . '/includes/Settings.php';
+require_once __DIR__ . '/includes/PageAs404Handler.php';
+require_once __DIR__ . '/includes/PageAs404ServiceProvider.php';
+
+// Boot plugin.
+new PageAs404\Inc\PageAs404ServiceProvider();
 
 // Register activation hook.
 register_activation_hook( __FILE__, array( 'PageAs404\Inc\Activation', 'activate' ) );
